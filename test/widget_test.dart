1b868236f0333db60e9ccdcf713bee1f6ec496ff
hjorthen1243 <114651351+hjorthen1243@users.noreply.github.com>
2024-02-29 10:12:46 +0100

    Test logic

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,24 +9,52 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:rpn_calculator/main.dart';
+import 'package:rpn_calculator/cal_logic.dart';
+
+var testClass = Calculator();
 
 void main() {
-    testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
-    /*
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add)); */
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-   /* expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget); */
-
+  group('PushCommand', () {
+    test('Push value to the stack', (){
+      testClass.stack = [1,2];
+      testClass.push(3);
+      expect(testClass.stack, [1,2,3]);
+    });
+  });
+  group('AddCommand', () {
+    test('Remove numbers and get result', (){
+      testClass.stack = [1, 2];
+      AddCommand().apply(testClass.stack);
+      expect(testClass.stack, [3]);
+    });
+  });
+  group('SubCommand', () {
+    test('Remove numbers and get result', (){
+      testClass.stack = [1, 2];
+      SubCommand().apply(testClass.stack);
+      expect(testClass.stack, [1]);
+    });
+  });
+  group('MultiCommand', () {
+    test('Remove numbers and get result', (){
+      testClass.stack = [3, 2];
+      MultiCommand().apply(testClass.stack);
+      expect(testClass.stack, [6]);
+    });
+  });
+  group('DivideCommand', () {
+    test('Remove numbers and get result', (){
+      testClass.stack = [2, 8];
+      DivideCommand().apply(testClass.stack);
+      expect(testClass.stack, [4]);
+    });
+  });
+  group('Calculate method', () {
+    test('Return string with result', (){
+      testClass.stack = [2 ,3, 5, 2, 3];
+      testClass.operation = [SubCommand(), MultiCommand(), AddCommand(), DivideCommand()];
+      var result = testClass.calculate();
+      expect(result, 4);
+    });
   });
 }
